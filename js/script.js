a6721b02dc008642b4175cccf09575c6b3e0db52
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2025-01-08 10:40:46 -0300

    closes #4 - alimentar por array

---
+++ js/script.js
@@ -0,0 +1,85 @@
+var clients = [ 
+    {
+        id: 1,
+        fullName: 'Maria Silva', 
+        logradouro: 'Rua tancredo Neves,144', 
+        cep: '18087-013', 
+        bairro: 'Centro', 
+        cidade: 'Sorocaba',
+        estado:'SP'
+    }, 
+    {
+        id: 1,
+        fullName: 'Marcos Silva', 
+        logradouro: 'Rua tancredo Neves,14', 
+        cep: '18087-013', 
+        bairro: 'Jardim imperador', 
+        cidade: 'São Paulo',
+        estado:'SP'
+    },  
+    {
+        id: 1,
+        fullName: 'João Silva', 
+        logradouro: 'Rua tancredo Neves,145', 
+        cep: '18087-015', 
+        bairro: 'Copacabana', 
+        cidade: 'Rio de Janeiro',
+        estado:'RJ'
+    },  
+];  
+
+loadClients(); 
+
+function loadClients(){ 
+    for(let client of clients){ 
+        addClient(client);
+    }
+}
+
+function addClient(client){ 
+    var table = document.getElementById("clientTable"); 
+    var newRow = table.insertRow();  
+
+    var idNode = document.createTextNode(client.id);            
+    var idCell= newRow.insertCell(); 
+    idCell.className='text-center';
+    idCell.appendChild(idNode); 
+    //newRow.insertCell().appendChild(idNode); 
+
+    var fullNameNode = document.createTextNode(client.fullName);            
+    var fullNameCell= newRow.insertCell(); 
+    fullNameCell.className='text-center';
+    fullNameCell.appendChild(fullNameNode); 
+
+    var logradouroNode = document.createTextNode(client.logradouro);            
+    var logradouroNameCell= newRow.insertCell(); 
+    logradouroNameCell.className='text-center';
+    logradouroNameCell.appendChild(logradouroNode); 
+
+    var cepNode = document.createTextNode(client.cep);            
+    var cepNameCell= newRow.insertCell(); 
+    cepNameCell.className='text-center';
+    cepNameCell.appendChild(cepNode); 
+
+    var bairroNode = document.createTextNode(client.bairro);            
+    var bairroNameCell= newRow.insertCell(); 
+    bairroNameCell.className='text-center';
+    bairroNameCell.appendChild(bairroNode); 
+
+    var cidadeNode = document.createTextNode(client.cidade);            
+    var cidadeCell= newRow.insertCell(); 
+    cidadeCell.className='text-center';
+    cidadeCell.appendChild(cidadeNode);
+  
+    var estadoNode = document.createTextNode(client.estado);            
+    var estadoCell= newRow.insertCell(); 
+    estadoCell.className='text-center';
+    estadoCell.appendChild(estadoNode);
+    // id: 1,
+    // fullName: 'João Silva', 
+    // logradouro: 'Rua tancredo Neves,145', 
+    // cep: '18087-015', 
+    // bairro: 'Centro', 
+    // cidade: 'Sorocaba',
+    // estado:'SP'
+}
